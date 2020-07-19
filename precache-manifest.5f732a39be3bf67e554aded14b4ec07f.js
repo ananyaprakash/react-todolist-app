501d3fd1d736b9748752cec57b0031332fcd88de
Ananya <ananya@Ananyas-Mac.local>
2020-07-19 12:20:44 +0530

    Updates

---
+++ precache-manifest.5f732a39be3bf67e554aded14b4ec07f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3265c1ec14142313132cf9982be8a3ef",
+    "url": "/react-todolist-app/index.html"
+  },
+  {
+    "revision": "7697188e6c0eb7664406",
+    "url": "/react-todolist-app/static/css/main.0d947773.chunk.css"
+  },
+  {
+    "revision": "5369231de70aa89e3314",
+    "url": "/react-todolist-app/static/js/2.47293ffc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist-app/static/js/2.47293ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7697188e6c0eb7664406",
+    "url": "/react-todolist-app/static/js/main.e3393f47.chunk.js"
+  },
+  {
+    "revision": "9738c57909327ec87c1e",
+    "url": "/react-todolist-app/static/js/runtime-main.4377dad2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
